e5bc7768b950ae92e72b591ca280cb7866598c1b
alexposseda <alexposseda@gmail.com>
2016-11-25 14:39:29 +0200

    add shop setting table

---
+++ console/migrations/m161125_123600_create_shop_setting_table.php
@@ -0,0 +1,36 @@
+<?php
+
+    use yii\db\Migration;
+
+    /**
+     * Handles the creation of table `{{%setting}}`.
+     */
+    class m161125_123600_create_shop_setting_table extends Migration{
+        protected $tableName = '{{%setting}}';
+
+        /**
+         * @inheritdoc
+         */
+        public function up(){
+            $tableOptions = null;
+            if($this->db->driverName === 'mysql'){
+                $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            }
+            $this->createTable($this->tableName, [
+                'id'         => $this->primaryKey(),
+                'key'        => $this->integer()
+                                     ->unique()
+                                     ->notNull(),
+                'value'      => $this->text(),
+                'created_at' => $this->integer(),
+                'updated_at' => $this->integer(),
+            ], $tableOptions);
+        }
+
+        /**
+         * @inheritdoc
+         */
+        public function down(){
+            $this->dropTable($this->tableName);
+        }
+    }
