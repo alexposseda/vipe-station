b344dd24b946d5370446c930a50085373bffef5f
alexposseda <alexposseda@gmail.com>
2016-11-25 14:03:02 +0200

    add manufacturer

--- console/migrations/m161123_094421_create_product_table.php
+++ console/migrations/m161123_094421_create_product_table.php
@@ -17,30 +17,35 @@
                 $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
             }
             $this->createTable($this->tableName, [
-                'id'            => $this->primaryKey(),
-                'title'         => $this->string()
-                                        ->notNull()
-                                        ->unique(),
-                'gallery'       => $this->text(),
-                'description'   => $this->text()
-                                        ->notNull(),
-                'base_price'    => $this->float()
-                                        ->notNull()
-                                        ->defaultValue(0),
-                'base_quantity' => $this->integer()
-                                        ->notNull()
-                                        ->defaultValue(0),
-                'slug'          => $this->string()
-                                        ->unique()
-                                        ->notNull(),
-                'sales'         => $this->integer()->defaultValue(0),
-                'views'         => $this->integer()->defaultValue(0),
-                'seo_id'        => $this->integer()->defaultValue(null),
-                'created_at'    => $this->integer(),
-                'updated_at'    => $this->integer(),
+                'id'              => $this->primaryKey(),
+                'title'           => $this->string()
+                                          ->notNull()
+                                          ->unique(),
+                'gallery'         => $this->text(),
+                'description'     => $this->text()
+                                          ->notNull(),
+                'base_price'      => $this->float()
+                                          ->notNull()
+                                          ->defaultValue(0),
+                'base_quantity'   => $this->integer()
+                                          ->notNull()
+                                          ->defaultValue(0),
+                'slug'            => $this->string()
+                                          ->unique()
+                                          ->notNull(),
+                'manufacturer_id' => $this->integer(),
+                'sales'           => $this->integer()
+                                          ->defaultValue(0),
+                'views'           => $this->integer()
+                                          ->defaultValue(0),
+                'seo_id'          => $this->integer()
+                                          ->defaultValue(null),
+                'created_at'      => $this->integer(),
+                'updated_at'      => $this->integer(),
             ], $tableOptions);
 
             $this->addForeignKey('FK_Product_TO_Seo', $this->tableName, 'seo_id', '{{%seo}}', 'id', 'SET NULL', 'CASCADE');
+            $this->addForeignKey('FK_Product_TO_Manufacturer', $this->tableName, 'manufacturer_id', '{{%manufacturer}}', 'id', 'SET NULL', 'CASCADE');
         }
 
         /**
@@ -48,6 +53,7 @@
          */
         public function down(){
             $this->dropForeignKey('FK_Product_TO_Seo', $this->tableName);
+            $this->dropForeignKey('FK_Product_TO_Manufacturer', $this->tableName);
             $this->dropTable($this->tableName);
         }
     }
