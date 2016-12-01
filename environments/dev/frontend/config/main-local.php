5e8f07eb864f36c42c75a3786b970689e8dd9821
SergSova <sergeysova@ukr.net>
2016-12-01 14:10:13 +0200

    main-local cache

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -6,6 +6,9 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
+        'cache' => [
+            'class' => 'yii\caching\DummyCache', //todo Поменять потом на реальное кеширование
+        ],
     ],
 ];
 
