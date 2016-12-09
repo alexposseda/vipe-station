141972bb2eb110a36079f9b1bee0400961bfee3d
gektorgit <gektor.gr@gmail.com>
2016-12-09 11:02:50 +0200

    coment productform event

--- common/components/sender/views/templates/text/passwordResetToken.php
+++ common/components/sender/views/templates/text/passwordResetToken.php
@@ -1,10 +1,14 @@
 <?php
     /* @var $this yii\web\View */
+    use yii\helpers\Html;
+
     /* @var $user common\models\UserIdentity */
     $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
     <?= Yii::t('mailer/messages', 'Hello') ?>
-        <?php if($user->client != null) {echo 'Html::encode($user->client->name)';}?>,
+        <?php if($user->client != null):?>
+    <?=Html::encode($user->client->name)?>
+    <?php endif;?>
     <?= Yii::t('mailer/messages', 'Follow the link below to reset your password') ?>:
 
     <?= $resetLink ?>
