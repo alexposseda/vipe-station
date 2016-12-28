5cb12b56c51db1d67ea9cb92fae69f792a3307d8
Alex Posseda <alexposseda@gmail.com>
2016-12-28 13:57:23 +0200

    fix view

--- backend/views/setting-shop/delivery-payment.php
+++ backend/views/setting-shop/delivery-payment.php
@@ -11,42 +11,54 @@
 ?>
 
 <?php $form = ActiveForm::begin() ?>
-<div class="panel panel-danger">
-    <span class="page-header"><?= Yii::t('shop/setting', 'Deliver') ?></span>
-    <div class="panel-body">
-        <div class="logo">
-            <?= Html::activeHiddenInput($model, 'logo[0]', ['id' => 'del-logo']) ?>
-            <?= FileManagerWidget::widget([
-                                              'uploadUrl'     => Url::to(['del-logo-upload']),
-                                              'removeUrl'     => Url::to(['del-logo-remove']),
-                                              'files'         => $model->logo[0],
-                                              'targetInputId' => 'del-logo',
-                                              'maxFiles'      => 1,
-                                              'title'         => $model->getAttributeLabel('logo')
-                                          ]) ?>
+<div class="row">
+    <div class="col-sm-12 col-md-6 col-lg-6">
+        <div class="panel panel-danger">
+            <div class="panel-heading">
+                <p class="panel-title"><?= Yii::t('shop/setting', 'Deliver') ?></p>
+            </div>
+            <div class="panel-body">
+                <div class="logo">
+                    <?= Html::activeHiddenInput($model, 'logo[0]', ['id' => 'del-logo']) ?>
+                    <?= FileManagerWidget::widget([
+                                                      'uploadUrl'     => Url::to(['del-logo-upload']),
+                                                      'removeUrl'     => Url::to(['del-logo-remove']),
+                                                      'files'         => $model->logo[0],
+                                                      'targetInputId' => 'del-logo',
+                                                      'maxFiles'      => 1,
+                                                      'title'         => $model->getAttributeLabel('logo')
+                                                  ]) ?>
+                </div>
+                <?= $form->field($model, 'title[0]') ?>
+                <?= $form->field($model, 'desc[0]')->textarea(['rows'=>6]) ?>
+            </div>
         </div>
-        <?= $form->field($model, 'title[0]') ?>
-        <?= $form->field($model, 'desc[0]') ?>
     </div>
-</div>
-<div class="panel panel-danger">
-    <span class="page-header"><?= Yii::t('shop/setting', 'Pay') ?></span>
-    <div class="panel-body">
-        <div class="logo">
-            <?= Html::activeHiddenInput($model, 'logo[1]', ['id' => 'pay-logo']) ?>
-            <?= FileManagerWidget::widget([
-                                              'uploadUrl'     => Url::to(['pay-logo-upload']),
-                                              'removeUrl'     => Url::to(['pay-logo-remove']),
-                                              'files'         => $model->logo[1],
-                                              'targetInputId' => 'pay-logo',
-                                              'maxFiles'      => 1,
-                                              'title'         => $model->getAttributeLabel('logo')
-                                          ]) ?>
+    <div class="col-sm-12 col-md-6 col-lg-6">
+        <div class="panel panel-danger">
+            <div class="panel-heading">
+                <p class="panel-title"><?= Yii::t('shop/setting', 'Pay') ?></p>
+            </div>
+            <div class="panel-body">
+                <div class="logo">
+                    <?= Html::activeHiddenInput($model, 'logo[1]', ['id' => 'pay-logo']) ?>
+                    <?= FileManagerWidget::widget([
+                                                      'uploadUrl'     => Url::to(['pay-logo-upload']),
+                                                      'removeUrl'     => Url::to(['pay-logo-remove']),
+                                                      'files'         => $model->logo[1],
+                                                      'targetInputId' => 'pay-logo',
+                                                      'maxFiles'      => 1,
+                                                      'title'         => $model->getAttributeLabel('logo')
+                                                  ]) ?>
+                </div>
+                <?= $form->field($model, 'title[1]') ?>
+                <?= $form->field($model, 'desc[1]')->textarea(['rows'=>6])  ?>
+            </div>
         </div>
-        <?= $form->field($model, 'title[1]') ?>
-        <?= $form->field($model, 'desc[1]') ?>
     </div>
 </div>
 
-<?= Html::submitButton(Yii::t('system', 'Save')) ?>
+
+
+<?= Html::submitButton(Yii::t('system', 'Save'), ['class'=>'btn btn-success']) ?>
 <?php ActiveForm::end() ?>
