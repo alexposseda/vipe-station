36577635b221b3e58a6e80d545df3e5e4395cdaf
SergSova <sergeysova@ukr.net>
2016-12-28 19:21:55 +0200

    seo grid view

--- backend/views/seo/index.php
+++ backend/views/seo/index.php
@@ -1,15 +1,15 @@
 <?php
 
-    use yii\helpers\Html;
-    use yii\grid\GridView;
-    use yii\widgets\Pjax;
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
 
-    /* @var $this yii\web\View */
-    /* @var $searchModel common\models\search\SeoSearchModel */
-    /* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\search\SeoSearchModel */
+/* @var $dataProvider yii\data\ActiveDataProvider */
 
-    $this->title = Yii::t('models', 'Seo');
-    $this->params['breadcrumbs'][] = $this->title;
+$this->title = Yii::t('models', 'Seo');
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="seo-model-index">
 
@@ -18,28 +18,34 @@
     <div class="row">
         <div class="col-md-12 col-lg-3">
             <p>
-                <?= Html::a(Yii::t('system/view', 'Create').' '.Yii::t('models', 'Seo'), ['create'], ['class' => 'btn btn-success']) ?>
+                <?= Html::a(Yii::t('system/view', 'Create') . ' ' . Yii::t('models', 'Seo'), ['create'], ['class' => 'btn btn-success']) ?>
                 <?= $this->render('_search', ['model' => $searchModel]); ?>
             </p>
         </div>
         <div class="col-md-12 col-lg-9">
             <?= GridView::widget([
-                                     'dataProvider' => $dataProvider,
-                                     'filterModel'  => $searchModel,
-                                     'columns'      => [
-                                         ['class' => 'yii\grid\SerialColumn'],
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => [
+                    ['class' => 'yii\grid\SerialColumn'],
 
-                                         'id',
-                                         'title',
-                                         //'keywords',
-                                         //'description',
-                                         //'seo_block:ntext',
-                                         'created_at:datetime',
-                                         'updated_at:datetime',
+                    'id',
+                    'title',
+                    [
+                        'label' => 'parent',
+                        'content' => function ($data) {
+                            return Html::a($data->getParent()->title, $data->link);
+                        }
+                    ],
+                    //'keywords',
+                    //'description',
+                    //'seo_block:ntext',
+                    //created_at:datetime',
+                    //'updated_at:datetime',
 
-                                         ['class' => 'yii\grid\ActionColumn'],
-                                     ],
-                                 ]); ?>
+                    ['class' => 'yii\grid\ActionColumn'],
+                ],
+            ]); ?>
         </div>
     </div>
     <?php Pjax::end(); ?>
--- common/models/SeoModel.php
+++ common/models/SeoModel.php
@@ -1,117 +1,147 @@
 <?php
 
-    namespace common\models;
+namespace common\models;
 
-    use Yii;
-    use yii\behaviors\TimestampBehavior;
-    use yii\db\ActiveRecord;
+use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\helpers\Url;
+
+/**
+ * This is the model class for table "{{%seo}}".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $keywords
+ * @property string $description
+ * @property string $seo_block
+ * @property integer $created_at
+ * @property integer $updated_at
+ *
+ * @property CategoryModel $category
+ * @property ProductModel $product
+ * @property BrandModel $brand
+ */
+class SeoModel extends ActiveRecord
+{
+    public $link;
 
     /**
-     * This is the model class for table "{{%seo}}".
-     *
-     * @property integer         $id
-     * @property string          $title
-     * @property string          $keywords
-     * @property string          $description
-     * @property string          $seo_block
-     * @property integer         $created_at
-     * @property integer         $updated_at
-     *
-     * @property CategoryModel[] $categories
-     * @property ProductModel[]  $products
-     * @property BrandModel[]    $brands
+     * @inheritdoc
      */
-    class SeoModel extends ActiveRecord{
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
+    }
 
-        /**
-         * @inheritdoc
-         */
-        public function behaviors(){
-            return [
-                TimestampBehavior::className(),
-            ];
-        }
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%seo}}';
+    }
 
-        /**
-         * @inheritdoc
-         */
-        public static function tableName(){
-            return '{{%seo}}';
-        }
-
-        /**
-         * @inheritdoc
-         */
-        public function rules(){
-            return [
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [
+                ['seo_block'],
+                'string'
+            ],
+            [
                 [
-                    ['seo_block'],
-                    'string'
+                    'created_at',
+                    'updated_at'
                 ],
+                'integer'
+            ],
+            [
                 [
-                    [
-                        'created_at',
-                        'updated_at'
-                    ],
-                    'integer'
+                    'title',
+                    'keywords'
                 ],
-                [
-                    [
-                        'title',
-                        'keywords'
-                    ],
-                    'string',
-                    'max' => 255
-                ],
-                [
-                    ['description'],
-                    'string',
-                    'max' => 500
-                ],
-            ];
+                'string',
+                'max' => 255
+            ],
+            [
+                ['description'],
+                'string',
+                'max' => 500
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => Yii::t('models/seo', 'Title'),
+            'keywords' => Yii::t('models/seo', 'Keywords'),
+            'description' => Yii::t('models/seo', 'Description'),
+            'seo_block' => Yii::t('models/seo', 'Seo Block'),
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(CategoryModel::className(), ['seo_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProduct()
+    {
+        return $this->hasOne(ProductModel::className(), ['seo_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBrand()
+    {
+        return $this->hasOne(BrandModel::className(), ['seo_id' => 'id']);
+    }
+
+    public function canSave()
+    {
+        if (!empty($this->title) or !empty($this->keywords) or !empty($this->description) or !empty($this->seo_block)) {
+            return true;
         }
 
-        /**
-         * @inheritdoc
-         */
-        public function attributeLabels(){
-            return [
-                'id'          => 'ID',
-                'title'       => Yii::t('models/seo', 'Title'),
-                'keywords'    => Yii::t('models/seo', 'Keywords'),
-                'description' => Yii::t('models/seo', 'Description'),
-                'seo_block'   => Yii::t('models/seo', 'Seo Block'),
-                'created_at'  => 'Created At',
-                'updated_at'  => 'Updated At',
-            ];
+        return false;
+    }
+
+    /**
+     * @return BrandModel|CategoryModel|ProductModel
+     */
+    public function getParent()
+    {
+        if ($this->product) {
+            $this->link = Url::to(['product/view', 'id' => $this->product->id]);
+            return $this->product;
         }
-
-        /**
-         * @return \yii\db\ActiveQuery
-         */
-        public function getCategories(){
-            return $this->hasMany(CategoryModel::className(), ['seo_id' => 'id']);
+        if ($this->category) {
+            $this->link = Url::to(['category/view', 'id' => $this->category->id]);
+            return $this->category;
         }
-
-        /**
-         * @return \yii\db\ActiveQuery
-         */
-        public function getProducts(){
-            return $this->hasMany(ProductModel::className(), ['seo_id' => 'id']);
-        }
-
-        /**
-         * @return \yii\db\ActiveQuery
-         */
-        public function getBrands(){
-            return $this->hasMany(BrandModel::className(), ['seo_id' => 'id']);
-        }
-
-        public function canSave(){
-            if(!empty($this->title) or !empty($this->keywords) or !empty($this->description) or !empty($this->seo_block)){
-                return true;
-            }
-
-            return false;
+        if ($this->brand) {
+            $this->link = Url::to(['brand/view', 'id' => $this->brand->id]);
+            return $this->brand;
         }
     }
+}
