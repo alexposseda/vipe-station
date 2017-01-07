24f0d8616710e6ecf4a6e7eb34b53f2b089dfbf9
Alex Posseda <alexposseda@gmail.com>
2017-01-08 01:15:34 +0200

    fix assets? create layout

---
+++ frontend/views/layouts/main.php
@@ -0,0 +1,123 @@
+<?php
+    /**
+     * @var $this    \yii\web\View
+     * @var $content string
+     */
+
+    use common\models\ShopSettingTable;
+    use frontend\assets\AppAsset;
+    use yii\alexposseda\fileManager\FileManager;
+    use yii\helpers\Html;
+    use yii\helpers\Url;
+
+    AppAsset::register($this);
+
+    $shopName = ShopSettingTable::getSettingValue('shopName');
+    if(empty($shopName)){
+        $shopName = Yii::$app->name;
+    }
+    $shopAddresses = ShopSettingTable::getSettingValue('address');
+    $shopBaseAddress = '';
+    $shopBasePhones = '';
+    $shopBaseSchedule = '';
+
+    if(!empty($shopAddresses)){
+        $shopAddresses = json_decode($shopAddresses);
+        foreach($shopAddresses as $s_a){
+            if($s_a->isGeneral == 1){
+                $shopBaseAddress = $s_a->address;
+                $shopBasePhones = $s_a->phones;
+                $shopBaseSchedule = $s_a->schedule;
+                break;
+            }
+        }
+    }
+
+    $socials = ShopSettingTable::getSettingValue('social');
+    if(!empty($socials)){
+        $socials = json_decode($socials);
+    }
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+    <header>
+        <div class="valign-wrapper">
+            <ul id="nav-mobile" class="side-nav fixed white-text valign" style="transform: translateX(0%);">
+                <li class="logo">
+                    <a id="logo" href="<?= Url::to(['site/index'])?>" class="brand-logo">
+                        <img class="logo" src="<?= Url::to('/images/logo.png', true)?>">
+                    </a>
+                </li>
+                <li class="menu">
+                    <ul class="col 112 nav-menu fs25 center-align">
+                        <li><a href="<?= Url::to(['site/about'])?>">О нас</a></li>
+                        <li><a href="<?= Url::to(['catalog'])?>">Каталог</a></li>
+                        <li><a href="<?= Url::to(['site/shipping-payment'])?>">Доставка и оплата</a></li>
+                        <li><a href="<?= Url::to(['site/shops'])?>">Наши магазины</a></li>
+                        <li>
+                            <hr>
+                        </li>
+                        <li class="hide-on-med-and-up">
+                            <a href="#modallogin" class="modal-trigger side-nav-modal">Кабинет</a>
+                        </li>
+                        <li class="hide-on-med-and-up">
+                            <hr>
+                        </li>
+                    </ul>
+                </li>
+                <li class="address-schedule">
+                    <div class="row valign-wrapper mb-10">
+                        <div class="col l6 m12 s12 valign left-align width">
+                            <span class="fs14"><?= $shopName?></span><br>
+                            <span class="fs14"><?= $shopBaseAddress?></span>
+                        </div>
+                        <div class="col l6 m12 valignleft-align width schedule">
+                            <div class="fs14">
+                                <?= nl2br($shopBasePhones)?> <br><?= $shopBaseSchedule?>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+<!--                <li class="subscribe">-->
+<!--                    <div class="row navbar-search">-->
+<!--                        <div class="col s12 m12 l12">-->
+<!--                            <form>-->
+<!--                                <div class="input-field">-->
+<!--                                    <input id="search" type="search" required>-->
+<!--                                    <input type="submit" alt="" class="plus" value="">-->
+<!--                                </div>-->
+<!--                            </form>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </li>-->
+                <li>
+                    <div class="row mb-0">
+                        <div class="col s12 m12 l12 center-align">
+                            <?php foreach($socials as $social):?>
+                                <a href="<?= $social->link?>" class="social-icon">
+                                    <img src="<?= FileManager::getInstance()->getStorageUrl().substr($social->icon, 1)?>" alt="<?= $social->title?>">
+                                </a>
+                            <?php endforeach;?>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <main>
+<?= $content ?>
+    </main>
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
