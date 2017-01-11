eef230a4aded6ff53768e15120ea2c3d44d87669
SergSova <sergeysova@ukr.net>
2017-01-11 13:30:34 +0200

    button to link

--- frontend/views/cart/_cartItem.php
+++ frontend/views/cart/_cartItem.php
@@ -22,10 +22,7 @@ use yii\helpers\Url;
             <div class="fs15 fc-dark-brown brand mb-5"><?= $model->product->brand->title ?></div>
         </div>
         <div class="col s2 right-align">
-            <?= Html::button('', [
-                'data-url' => Url::to(['delete', 'id' => $model->id]),
-                'class' => 'delete-product'
-            ]) ?>
+            <?= Html::a('', Url::to(['delete', 'id' => $model->id]),['class' => 'delete-product']) ?>
         </div>
         <div class="product-total col s12">
             <div class="count-yoy-order left fc-brown"
