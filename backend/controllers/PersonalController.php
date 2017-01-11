a30efa75f3eb59749decb115f7bf744d036b5bc3
SergSova <sergeysova@ukr.net>
2017-01-11 10:53:30 +0200

    personal controller admin rule

--- backend/controllers/PersonalController.php
+++ backend/controllers/PersonalController.php
@@ -3,11 +3,40 @@
 namespace backend\controllers;
 
 
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
 use yii\web\Controller;
+use yii\web\UnauthorizedHttpException;
 
 class PersonalController extends Controller
 {
-
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'denyCallback' => function($rule, $action){
+                    throw new UnauthorizedHttpException(Yii::t('system/error', 'You do not have access to this page'));
+                },
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['admin'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
     public function actionIndex()
     {
         return $this->render('dashboard');
