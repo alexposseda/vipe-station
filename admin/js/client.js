6ada2db54694c3cd0166b58592560e647fd85125
gektorgit <gektor.gr@gmail.com>
2017-01-24 17:24:25 +0200

    add delivery address

--- admin/js/client.js
+++ admin/js/client.js
@@ -1,42 +1,43 @@
 /**
  * Created by gektorgit on 12.12.16.
  */
-$('button.add-delivery').click(function () {
+$('button#add-delivery').click(function () {
     var index = $('.address-input').children('.form-inline').length;
     var html = '<div class="form-inline">' +
-        '<div class="form-group field-clientmodel-delivery_data_arr-' + index + '-f_name has-success">' +
-        '<input type="text" id="clientmodel-delivery_data_arr-' + index + '-f_name" class="form-control" name="ClientModel[delivery_data_arr][' + index + '][f_name]" placeholder="Имя">' +
+        '<div class="form-group field-deliveryaddressform-' + index + '-f_name required">' +
+        '<input type="text" id="deliveryaddressform-' + index + '-f_name" class="form-control" name="DeliveryAddressForm[' + index + '][f_name]" placeholder="Имя">' +
         '<div class="help-block"></div>' +
         '</div>' +
-        '<div class="form-group field-clientmodel-delivery_data_arr-' + index + '-l_name">' +
-        '<input type="text" id="clientmodel-delivery_data_arr-' + index + '-l_name" class="form-control" name="ClientModel[delivery_data_arr][' + index + '][l_name]" placeholder="Фамилия">' +
+        '<div class="form-group field-deliveryaddressform-' + index + '-l_name required">' +
+        '<input type="text" id="deliveryaddressform-' + index + '-l_name" class="form-control" name="DeliveryAddressForm[' + index + '][l_name]" placeholder="Фамилия">' +
         '<div class="help-block"></div>' +
         '</div>' +
-        '<div class="form-group field-clientmodel-delivery_data_arr-' + index + '-city">' +
-        '<input type="text" id="clientmodel-delivery_data_arr-' + index + '-city" class="form-control" name="ClientModel[delivery_data_arr][' + index + '][city]" placeholder="Город">' +
+        '<div class="form-group field-deliveryaddressform-' + index + '-city required">' +
+        '<input type="text" id="deliveryaddressform-' + index + '-city" class="form-control" name="DeliveryAddressForm[' + index + '][city]" placeholder="Город">' +
         '<div class="help-block"></div>' +
         '</div>' +
-        '<div class="form-group field-clientmodel-delivery_data_arr-' + index + '-address">' +
-        '<input type="text" id="clientmodel-delivery_data_arr-' + index + '-address" class="form-control" name="ClientModel[delivery_data_arr][' + index + '][address]" placeholder="Адрес">' +
+        '<div class="form-group field-deliveryaddressform-' + index + '-address required">' +
+        '<input type="text" id="deliveryaddressform-' + index + '-address" class="form-control" name="DeliveryAddressForm[' + index + '][address]" placeholder="Адрес">' +
         '<div class="help-block"></div>' +
         '</div>' +
-        '<div class="form-group field-clientmodel-delivery_data_arr-' + index + '-phone">' +
-        '<input type="text" id="clientmodel-delivery_data_arr-' + index + '-phone" class="form-control" name="ClientModel[delivery_data_arr][' + index + '][phone]" data-plugin-inputmask="inputmask_c6d7b205">' +
-        '<div class="help-block"></div>' +
-        '</div>' +
-        '</div>';
+        '<div class="form-group field-deliveryaddressform-'+index+'-phone required">'+
 
-    $('div.address-input').parent().append(html);
-    alert("Hello");
-    // var delivery_data = '{
-    // "firstName": $('input[name="firstName"]').val(),
-    //     'lastName' = $('input[name="lastName"]').val();
-    // var city = $('input[name="city"]').val();
-    // var address = $('input[name="address"]').val();
-    // var phone = $('input[name="phone"]').val();
-    // }';
+        '<input type="text" id="deliveryaddressform-'+index+'-phone" class="form-control" name="DeliveryAddressForm['+index+'][phone]" data-plugin-inputmask="inputmask_c6d7b205">'+
 
-    // $('#clientmodel-delivery_data').val();
+        '<div class="help-block"></div>'+
+        '</div>'+
+
+        '<button type="button" class="btn btn-sm btn-danger del-delivery" data-index="' + index + '"><span class="glyphicon glyphicon-remove"></span></button>'
+    '</div>';
+
+    $('div.address-input').append(html);
+
+    $('.del-delivery').on('click', deleteDelivery);
 
 })
-;
+$('.del-delivery').on('click', deleteDelivery);
+function deleteDelivery(){
+
+    console.log($(this).parent());
+    $(this).parent().remove();
+}
